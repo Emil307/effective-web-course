837b8833d9a36f8b8dd963fc11f2e65ee9adc790
Emil307 <emilnovikov392@gmail.com>
2022-12-02 09:09:12 +0600

    add item pages

---
+++ lab4/react-ts/src/components/Header.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components'; 
+import { NavLink } from 'react-router-dom';
+
+const Head = styled.header`
+    padding: 15px;
+    background-color: var(--header-bg-color);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+const Image = styled.img`
+    height: 30px;
+`
+
+const Nav = styled.nav`
+    display: flex;
+    justify-content: space-between;
+    width: 240px;
+`
+
+const Link = styled.a`
+    font-size: 18px;
+    color: var(--primary-color);
+    cursor: pointer;    
+`
+
+const Header = () => {
+  return (
+    <Head>
+        <Image src="../../logo.svg" alt="Header Logo" />
+        <Nav>
+            <NavLink to="/"><Link>Characters</Link></NavLink>
+            <NavLink to="/comics"><Link>Comics</Link></NavLink>
+            <NavLink to="/series"><Link>Series</Link></NavLink>
+        </Nav>
+    </Head>
+  )
+}
+
+export default Header;
---
+++ lab4/react-ts/src/data/series.ts
@@ -0,0 +1,46 @@
+import { ISeries } from "../models";
+
+export const series: ISeries[] = [
+    {
+        "id":1,
+        "type": "series",
+        "title":"Abdul Alhazred",
+        "description":"Abd-el-Hazred",
+        "image":"https://terrigen-cdn-dev.marvel.com/content/prod/1x/antmanandthewaspquantumania_lob_crd_02.jpg",
+    },
+    {
+        "id":2,
+        "type": "series",
+        "title":"Aberration",
+        "description":"Rana Philips",
+        "image":"https://terrigen-cdn-dev.marvel.com/content/prod/1x/blackpantherwakandaforever_lob_crd_05.jpg",
+    },
+    {
+        "id":3,
+        "type": "series",
+        "title":"Aberration",
+        "description":"Rana Philips",
+        "image":"https://terrigen-cdn-dev.marvel.com/content/prod/1x/doctorstrangeinthemultiverseofmadness_lob_crd_02_3.jpg",
+    },
+    {
+        "id":4,
+        "type": "series",
+        "title":"Aberration",
+        "description":"Rana Philips",
+        "image":"https://terrigen-cdn-dev.marvel.com/content/prod/1x/captainamericathewintersoldier_lob_crd_01_1.jpg",
+    },
+    {
+        "id":5,
+        "type": "series",
+        "title":"Aberration",
+        "description":"Rana Philips",
+        "image":"https://terrigen-cdn-dev.marvel.com/content/prod/1x/thorthedarkworld_lob_crd_02_1.jpg",
+    },
+    {
+        "id":6,
+        "type": "series",
+        "title":"Aberration",
+        "description":"Rana Philips",
+        "image":"https://terrigen-cdn-dev.marvel.com/content/prod/1x/spider-manhomecoming_lob_crd_01_3.jpg",
+    },
+]
